eb4d6a4b05534bba12616fdc60f7ef4e0689ed61
ST10467193_Tristan_Kriel_BCA1_PROG5112_GR02 <krieltristan68@gmail.com>
2025-09-29 14:27:02 +0200

    Added Question 2: Cricketer application

---
+++ src/PROG6112_Question2/PROG6112_Question2.java
@@ -0,0 +1,11 @@
+package PROG6112_Question2;
+
+
+public class PROG6112_Question2 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
