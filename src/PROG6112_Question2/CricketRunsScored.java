12e415b2e7924bb5140eca6e4294b32e03880b47
ST10467193_Tristan_Kriel_BCA1_PROG5112_GR02 <krieltristan68@gmail.com>
2025-09-29 14:30:29 +0200

    Added Question 2: Cricketer application

---
+++ src/PROG6112_Question2/CricketRunsScored.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PROG6112_Question2;
+
+/**
+ *
+ * @author kriel
+ */
+public class CricketRunsScored extends Cricket {
+    
+}
